792ba9e9fd6b316c8900018bb0670652c3202501
Caleb Steinbaugh <calebsteinbaugh@gmail.com>
2026-04-09 11:27:59 -0500

    Add files via upload

---
+++ television.py
@@ -0,0 +1,46 @@
+class Television:
+    min_volume = 0
+    max_volume = 2
+    min_channel = 0
+    max_channel = 3
+
+    def __init__(self, status: bool = False, muted: bool = False, volume: int = min_volume, channel: int = min_channel):
+
+        self.__status = status
+        self.__muted = muted
+        self.__volume = volume
+        self.__channel = channel
+
+    def power(self):
+        self.__status = not self.__status 
+
+    def mute(self):
+        self.__muted = not self.__muted  
+
+    def channel_up(self):
+        if self.__channel == Television.max_channel:
+            self.__channel = Television.min_channel
+        else:
+            self.__channel += 1
+
+    def channel_down(self):
+        if self.__channel == Television.min_channel:
+            self.__channel = Television.max_channel
+        else:
+            self.__channel -= 1
+
+    def volume_up(self):
+        if self.__muted:
+            self.__muted = not self.__muted
+            
+        if self.__volume < Television.max_volume:
+            self.__volume += 1
+
+    def volume_down(self):
+        if self.__muted:
+            self.__muted = not self.__muted     
+        if self.__volume > Television.min_volume:
+            self.__volume -= 1
+
+    def __str__(self):
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
